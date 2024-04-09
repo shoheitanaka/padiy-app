35c191fe8a0c06aef46544ec0e72a297660694eb
Shohei Tanaka <st.ryoma@gmail.com>
2024-04-09 16:06:49 +0900

    update config box

--- config/box.php
+++ config/box.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'clientId'       => env('zt5la97gtt896wosti8x62rky9f9mow9'),
-    'clientSecret'   => env('bODgLiDVfWhFku94WzkZSDcf8NdNeOI4'),
-    'redirectUri'    => env('https://paidy.artws.info/box/oauth'),
-    'boxLandingUri'  => env('https://paidy.artws.info/box'),
+    'clientId'       => 'zt5la97gtt896wosti8x62rky9f9mow9',
+    'clientSecret'   => 'bODgLiDVfWhFku94WzkZSDcf8NdNeOI4',
+    'redirectUri'    => 'https://paidy.artws.info/box/oauth',
+    'boxLandingUri'  => 'https://paidy.artws.info/box',
     'urlAuthorize'   => 'https://account.box.com/api/oauth2/authorize',
     'urlAccessToken' => 'https://www.box.com/api/oauth2/token',
 ];
