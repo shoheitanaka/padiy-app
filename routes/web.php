b1442a3edcf9a5ddc404e23523c7406065115fc3
Shohei Tanaka <st.ryoma@gmail.com>
2024-04-09 18:12:04 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -49,8 +49,7 @@ Route::get( 'box', function() {
     $file_id = '1444821902061';
     $filepath = storage_path('box_data').'/woocommerce_merchant_list.xlsx';
     $name = 'woocommerce_merchant_list.xlsx';
-    $new_name = 'woocommerce merchant list Base.xlsx';
-    return Box::files()->uploadRevision( $file_id, $filepath, $name, $new_name );
+    return Box::files()->uploadRevision( $file_id, $filepath, $name );
 
     //example of getting the authenticated users details
 //    return Box::get('/users/me');
