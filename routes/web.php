540de526f492b423f6579e0ec15e3c5d0e0ab564
Shohei Tanaka <st.ryoma@gmail.com>
2024-04-09 17:05:07 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -46,10 +46,10 @@ Route::get( 'box', function() {
     Box::getAccessToken();
 
     //box authenticated now box:: can be used freely.
-    $path = storage_path('box_data').'/woocommerce_merchant_list.xlsx';
+    $file_id = '1444821902061';
+    $filepath = storage_path('box_data').'/woocommerce_merchant_list.xlsx';
     $name = 'woocommerce merchant list Base.xlsx';
-    Box::files()->upload( $path, $name );
-
+    Box::files()->uploadRevision( $file_id, $filepath, $name );
 
     //example of getting the authenticated users details
     return Box::get('/users/me');
