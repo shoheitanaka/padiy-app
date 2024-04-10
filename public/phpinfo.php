767aa910a335b391270b37e8f7b4e9fc3e7186f4
Shohei Tanaka <st.ryoma@gmail.com>
2024-04-10 11:37:01 +0900

    Update phpinfo.php

--- public/phpinfo.php
+++ public/phpinfo.php
@@ -3,4 +3,4 @@
 $filepath = '/home/kusanagi/paidy_artws/padiy-app/storage/box_data/photo.png';
 $name = 'photo.png';
 $data = new \CurlFile($filepath, mime_content_type($filepath), $name);
-echo $data;
+//echo $data;
