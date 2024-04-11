9f7d83a257026b1368fcd9641ea261981b2d1a28
Shohei Tanaka <st.ryoma@gmail.com>
2024-04-11 10:47:49 +0900

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -28,4 +28,18 @@
             </div>
         </div>
     </div>
+    <div class="py-12 container">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 row">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg w-50 col mr-4">
+                <div class="p-6 text-gray-900">
+                    <a href="/box" class="btn btn-primary">BOX更新</a>
+                </div>
+            </div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg w-50 col">
+<!--                <div class="p-6 text-gray-900">
+                    <a href="/import-csv" class="btn btn-primary">インポート</a>
+                </div>-->
+            </div>
+        </div>
+    </div>
 </x-app-layout>
