5b16987fb9265ae497b08cf6c49db00543c0bdcd
Shohei Tanaka <st.ryoma@gmail.com>
2024-05-21 06:15:27 +0900

    Fixed Paidy connect

--- app/Http/Controllers/PaidyConnectController.php
+++ app/Http/Controllers/PaidyConnectController.php
@@ -9,8 +9,8 @@ use Illuminate\Http\Request;
 class PaidyConnectController extends Controller
 {
     public function paidy_sftp_upload() {
-        $file = new File(storage_path('box_data/').'woocommerce_merchant_list.xlsx');
-        if(Storage::disk('sftp')->put('woocommerce_merchant_list.xlsx', $file)){
+        $file = new File( storage_path('box_data').'/woocommerce_merchant_list.xlsx' );
+        if(Storage::disk('sftp')->put('home/woocommerce_merchant_list.xlsx', $file)){
             Log::debug('The file was successfully transferred to Paidy\'s server.');
             return true;
         }else{
