871fa47c27437942fc91a355eeeeb88ac1428a24
Shohei Tanaka <st.ryoma@gmail.com>
2024-07-03 15:28:12 +0900

    Fixed Schedule data

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,14 +16,14 @@ class Kernel extends ConsoleKernel
     {
         $schedule->call(function () {
             Log::debug('Schedule Start.');
-            $update_file = Storage::disk('local')->get( 'public/woocommerce_merchant_list.csv' );
+            $update_file = Storage::disk('local')->get( 'woocommerce_merchant_list.csv' );
             $result = Storage::disk('sftp')->put('woocommerce_merchant_list.csv', $update_file);
             if($result){
                 Log::debug('The file was successfully transferred to Paidy\'s server.');
             }else{
                 Log::error('File transfer to Paidy\'s server failed.');
             }
-        })->everySixHours();
+        })->everyFiveMinutes();
     }
 
     /**
