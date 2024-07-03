4ca1e755910f19fb0d54ff2c4a5b789eb9e3b0b5
Shohei Tanaka <st.ryoma@gmail.com>
2024-07-03 12:03:55 +0900

    Update Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,8 +16,8 @@ class Kernel extends ConsoleKernel
     {
         $schedule->call(function () {
             Log::debug('Schedule Start.');
-            $update_file = Storage::disk('local')->get( 'public/woocommerce_merchant_list.xlsx' );
-            $result = Storage::disk('sftp')->put('woocommerce_merchant_list.xlsx', $update_file);
+            $update_file = Storage::disk('local')->get( 'public/woocommerce_merchant_list.csv' );
+            $result = Storage::disk('sftp')->put('woocommerce_merchant_list.csv', $update_file);
             if($result){
                 Log::debug('The file was successfully transferred to Paidy\'s server.');
             }else{
